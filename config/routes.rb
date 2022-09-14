fb0833f729456f80a780121df4b1a57e7f176f8c
reizyte <112692614+reizyte@users.noreply.github.com>
2022-09-14 13:51:12 +0000

    4-19

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   resources :post_images,only:[:new, :show, :index, :create, :destroy] do
+   resource :favorites, only: [:create, :destroy]
    resources :post_comments, only: [:create, :destroy]
   end
   resources :users,only:[:show, :edit, :update]
