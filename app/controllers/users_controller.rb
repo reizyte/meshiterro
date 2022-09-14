a4a1abb80168cefd9d64aeeee4b55e5e738990b3
reizyte <112692614+reizyte@users.noreply.github.com>
2022-09-14 09:38:54 +0000

    4-18

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @post_images = @user.post_image
+    @post_images = @user.post_images
   end
 
   def edit
@@ -19,7 +19,6 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
-  
   end
   
  
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   
   devise_for :users
   root to: "homes#top"
-  resources :post_images,only:[:new, :show, :index, :create, :destroy]
+  resources :post_images,only:[:new, :show, :index, :create, :destroy] do
+   resources :post_comments, only: [:create, :destroy]
+  end
   resources :users,only:[:show, :edit, :update]
   get "homes/about" => "homes#about",as:'about'
   
