779abb75b41c4eef0c7eed5a5c57f268ed212dcf
reizyte <112692614+reizyte@users.noreply.github.com>
2022-09-15 09:25:50 +0000

    4-24

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @post_images = @user.post_images
+    @post_images = @user.post_images.page(params[:page])
   end
 
   def edit
@@ -19,6 +19,6 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
-  end
+end
   
  
